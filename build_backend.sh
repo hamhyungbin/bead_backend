30f08cd1977ecfb0e3d3211f29e21b59f36ff37d
Ham Hyungbin <35593401+hamhyungbin@users.noreply.github.com>
2025-06-02 16:32:44 +0900

    Update build_backend.sh

--- build_backend.sh
+++ build_backend.sh
@@ -4,4 +4,4 @@ set -o errexit
 
 pip install -r requirements.txt
 # Run database migrations if you have them and want auto-migration
-# flask db upgrade # Ensure Flask-Migrate is set up
+flask db upgrade # Ensure Flask-Migrate is set up
